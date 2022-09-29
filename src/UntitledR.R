751d8571e1d983eaf70907b7964ac10ba05f3699
Kucing Kurus <kucingkurus@pop-os.localdomain>
2022-09-29 17:07:28 +0700

    major changes, pivot, etc

--- src/UntitledR.R
+++ src/UntitledR.R
@@ -33,11 +33,31 @@ penjaluran <- read.csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Data
   clean_names()
 
 dataBongkar <- read.csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Databases/ba_bongkar.csv") %>%
+  clean_names() %>%
+  mutate(tanggal_ba_buka_segel = as.Date(tanggal_ba_buka_segel)) %>%
+  mutate(berat = as.integer(berat))
+
+dataTangkapan <- read.csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Databases/data_tangkapan.csv") %>%
   clean_names()
 
-volumebongkar <- aggregate(dataBongkar["total"], by=dataBongkar["tanggal_ba_buka_segel"], sum)
+volumebongkar <- aggregate(list(jumlahkoli=dataBongkar$total, berat=dataBongkar$berat), by=dataBongkar["tanggal_ba_buka_segel"], sum)
+
+jml_sbp_npp <- dataTangkapan %>%
+  drop_na(no_sbp) %>%
+  nrow() %>%
+  as.numeric()
 
 x <- c(1:100)
 random_y <- rnorm(100, mean = 0)
 data <- data.frame(x, random_y)
+
+
+##jumlah asal  truk
+
+jml_bkr_asal_raw <- dataBongkar %>%
+  count(asal, tanggal_ba_buka_segel) %>%
+  pivot_wider(names_from = asal, values_from = n) %>%
+  arrange(tanggal_ba_buka_segel)
+  
+  jml_bkr_asal_raw[is.na(jml_bkr_asal_raw)] = 0
 
--- src/app.R
+++ src/app.R
@@ -42,9 +42,26 @@ ui <- dashboardPage(
       tabItem(tabName = "dashboardP2",
               fluidRow(
                 box(title = strong("Dashboard Penindakan dan Penyidikan 2022"), status = "info", strong("kucingkurusmandi"), p("di papan"), width = 12),
-                box(title = "SBP 2022", status = "primary", width = 6, plotlyOutput("negaraAsalSBP2022")),
+                ### laporan Bongkar
+                box(title = strong("Laporan Bongkar"), status = "info", width = 12),
+                box(title =  "BA Bongkaran", status = "info", width = 12, plotlyOutput("volumeBongkaran")),
+                box(title = "keterangan", status = "info", width = 12, strong("asal"),plotlyOutput("render_jml_bkr_asal")),
+                box(title = "datarange tanggal tes", status = "info", width = 3, dateRangeInput("dateRange1", "tanggal:",
+                                                                                              start = Sys.Date(),
+                                                                                              end = Sys.Date(),
+                                                                                              min = "2022-01-01",
+                                                                                              max = Sys.Date(),
+                                                                                              format = "yyyy-mm-dd",
+                                                                                              separator = "-")),
+                ### laporan pemeriksaan
+                box(title = strong("Laporan Pemeriksaan"), status = "info", width = 12),
                 box(title = "penjaluran", status = "success", width = 6, plotlyOutput("progresPenjaluran")),
-                box(title =  "BA Bongkaran", status = "info", width = 6, plotlyOutput("volumeBongkaran"))
+                ### laporan pengajuan LAb
+                box(title = strong("Laporan Pengajuan lab"), status = "info", width = 12),
+                ### laporan SBP
+                box(title = strong("Jumlah SBP"), status = "info", width = 12),
+                box(title = "SBP 2022", status = "primary", width = 6, plotlyOutput("negaraAsalSBP2022")),
+                box(title = "jumlah SBP", status = "info", width = 3, strong("NON NPP"), valueBoxOutput("jml_sbp_non"), strong("NPP"), valueBoxOutput("jml_sbp_npp"))
               )),
       tabItem(tabName = "tabelSBP2022",
             fluidRow(
@@ -61,8 +78,6 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
  #load data
   
-  ## sbp2021 <- read_csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Databases/sbp_2021.csv")
-  
   sbp2022 <- read.csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Databases/SBP_2022.csv") %>%
     clean_names()
   
@@ -70,8 +85,13 @@ server <- function(input, output, session) {
     clean_names()
   
   dataBongkar <- read.csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Databases/ba_bongkar.csv") %>%
-    clean_names()
+    clean_names() %>%
+    mutate(tanggal_ba_buka_segel = as.Date(tanggal_ba_buka_segel)) %>%
+    mutate(berat = as.integer(berat))
   
+  dataTangkapan <- read.csv2("/home/kucingkurus/Documents/Web Development/siPeDa/Databases/data_tangkapan.csv") %>%
+    clean_names()
+
   #panggil data sbp untuk menjadi tabel 
   
   output$tabelSBP2022 <- renderDataTable({sbp2022})
@@ -99,14 +119,83 @@ server <- function(input, output, session) {
       add_trace(y= ~total_hijau, name = 'jumlah cn', type = 'scatter', mode = 'lines', line = list(color = 'rgb(124, 252, 0)'))
   })
   
-  #line chart bongkaran
+  # value total SBP
   
-  volumeBongkar <- aggregate(dataBongkar["total"], by=dataBongkar["tanggal_ba_buka_segel"], sum)
+  jml_sbp_non_val <- sbp2022 %>% 
+    drop_na(no_kiriman) %>%
+    nrow() %>%
+    as.numeric()
+  
+  jml_sbp_npp_val <- dataTangkapan %>%
+    drop_na(no_sbp) %>%
+    nrow() %>%
+    as.numeric()
+  
+  output$jml_sbp_non <- renderValueBox({
+    valueBox(
+      jml_sbp_non_val
+    )
+  })
+  
+  output$jml_sbp_npp <- renderValueBox({
+    valueBox(
+      jml_sbp_npp_val
+    )
+  })
+  
+  # laporan bongkar
+  
+  ##line chart bongkaran
+  
+  volumeBongkar <-  aggregate(list(jumlahKoli=dataBongkar$total, berat=dataBongkar$berat), by=dataBongkar["tanggal_ba_buka_segel"], sum)
   
   output$volumeBongkaran <- renderPlotly({
     plot_ly(volumeBongkar, x = ~ tanggal_ba_buka_segel) %>%
-      add_trace(y= ~total, name = 'Volume Bongkaran', type = 'scatter', mode = 'lines', line = list(color = 'rgb(124, 252, 0)'))
+      add_trace(y= ~jumlahKoli, name = 'Jumlah Koli', type = 'scatter', mode = 'lines', line = list(color = 'rgb(124, 252, 0)')) %>%
+      add_trace(y= ~berat, name = 'Berat Paket (Kg)', type = 'scatter', mode = 'lines', line = list(color = 'rgb(255, 0, 0)')) %>%
+      layout(title='Berita Acara Bongkar',
+             xaxis = list(title = "Tanggal", rangeslider = list(visible = T),
+                          rangeselector=list(
+                            buttons=list(
+                              list(count=1, label="1m", step="month", stepmode="backward"),
+                              list(count=6, label="6m", step="month", stepmode="backward"),
+                              list(count=1, label="YTD", step="year", stepmode="todate"),
+                              list(count=1, label="1y", step="year", stepmode="backward"),
+                              list(step="all")
+                            ))),
+             yaxis = list(title = "Volume"))
   })
+  
+  
+  ## grafik jumlah asal  truk
+  
+  jml_bkr_asal <- dataBongkar %>%
+    count(asal, tanggal_ba_buka_segel) %>%
+    pivot_wider(names_from = asal, values_from = n) %>%
+    arrange(tanggal_ba_buka_segel)
+  
+  jml_bkr_asal[is.na(jml_bkr_asal)] = 0
+  
+  output$render_jml_bkr_asal <- renderPlotly({
+    plot_ly(jml_bkr_asal, x = ~ tanggal_ba_buka_segel) %>%
+      add_trace(y= ~` SH `, name = 'Soekarno-Hatta', type = 'scatter', mode = 'lines', line = list(color = 'rgb(0, 0, 255)')) %>%
+      add_trace(y= ~` PRIOK `, name = 'Tanjung Priok', type = 'scatter', mode = 'lines', line = list(color = 'rgb(255, 0, 0)')) %>%
+      add_trace(y= ~` SH (ECOMMERCE) `, name = 'E-Commerce', type = 'scatter', mode = 'lines', line = list(color = 'rgb(0, 255, 0)')) %>%
+      layout(title='Berita Acara Bongkar',
+             xaxis = list(title = "Tanggal", rangeslider = list(visible = T),
+                          rangeselector=list(
+                            buttons=list(
+                              list(count=1, label="1m", step="month", stepmode="backward"),
+                              list(count=6, label="6m", step="month", stepmode="backward"),
+                              list(count=1, label="YTD", step="year", stepmode="todate"),
+                              list(count=1, label="1y", step="year", stepmode="backward"),
+                              list(step="all")
+                            ))),
+             yaxis = list(title = "Jumlah Truk"))
+  })
+  
+  
 }
+
 
 shinyApp(ui, server)
